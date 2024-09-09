ebaeca3a1dad350bd5d3c23cb2d6062b2a5fc350
MaritzaTC <maritza.tabarezc@udea.edu.co>
2024-09-09 13:53:29 -0500

    feat(project):🎉 initialize concessionaire project                                                                                                    Co-authored-by: anavegaa <ana.vega@udea.edu.co>  Co-authored-by: JuanVasquezO <juan.vasquez21@udea.edu.co>

---
+++ src/main/java/com/udea/webconcesionaire/entity/PhotoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.webconcesionaire.entity;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author marit
+ */
+@Stateless
+public class PhotoFacade extends AbstractFacade<Photo> {
+
+    @PersistenceContext(unitName = "com.udea_WEBConcesionaire_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PhotoFacade() {
+        super(Photo.class);
+    }
+    
+}
